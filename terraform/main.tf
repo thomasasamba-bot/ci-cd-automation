30a204f503e5c3cfc7a787b477e1c67f719bd3fe
Thomas Asamba <asamba.thomas@gmail.com>
2026-01-13 16:09:58 +0300

    Refactor to standard Rails Docker setup

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "rails-lambda-connector" # Keeping same name or change to rails-app
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
